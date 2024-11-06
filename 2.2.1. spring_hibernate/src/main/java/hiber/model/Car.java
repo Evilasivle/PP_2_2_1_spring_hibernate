76291f6ed95b7a0371ab30dc067d9ec909b64376
Evilasivle <evilasivle@gmail.com>
2024-11-06 14:48:05 +0300

    Наводил порядок с Transactional, менял методы

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,91 @@
+package hiber.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "series")
+    private int series;
+
+    @Column(name = "model")
+    private String model;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Car(){}
+
+    public Car(int series, String model) {
+        this.series = series;
+        this.model = model;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public User setUser(User user) {
+        this.user = user;
+        return user;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return getSeries() == car.getSeries() && Objects.equals(getModel(), car.getModel()) && Objects.equals(getId(), car.getId()) && Objects.equals(getUser(), car.getUser());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSeries(), getModel(), getId(), getUser());
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "series=" + series +
+                ", model='" + model + '\'' +
+                ", id=" + id +
+                ", user=" + user.getFirstName() +
+                '}';
+    }
+}
